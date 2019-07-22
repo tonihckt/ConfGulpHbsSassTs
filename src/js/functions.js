0d70d66d4d572c79a9970aaa6f0c1f9bdd1e4a9f
Toni Hackett <hckt76@gmail.com>
2019-07-23 00:46:47 +0200

    marckp-hbs

--- src/js/functions.js
+++ src/js/functions.js
@@ -8,13 +8,14 @@
 // main.init();
 
 console.log('Hola! soyjjhh');
+// import('slider.js');
 
 // greeting function
-(function(){
-    var theScriptHtml = document.getElementById('greeting-template').innerHTML;
-    var theTemplate = Handlebars.compile(theScriptHtml)
-    var contextObj = {name:"Toni", city:"Barcelona"};
-    var compiledData = theTemplate(contextObj);
+// (function(){
+//     var theScriptHtml = document.getElementById('greeting-template').innerHTML;
+//     var theTemplate = Handlebars.compile(theScriptHtml)
+//     var contextObj = {name:"Toni", city:"Barcelona"};
+//     var compiledData = theTemplate(contextObj);
 
-    document.getElementById('greeting').innerHTML = compiledData
-})
+//     document.getElementById('greeting').innerHTML = compiledData
+// })
