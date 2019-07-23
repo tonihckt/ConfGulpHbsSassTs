3c9ae31009d47868b733290dc4bb9a71e58d5503
Toni Hackett <hckt76@gmail.com>
2019-07-23 11:31:29 +0200

    struc-hbs

--- src/js/functions.js
+++ src/js/functions.js
@@ -19,3 +19,31 @@ console.log('Hola! soyjjhh');
 
 //     document.getElementById('greeting').innerHTML = compiledData
 // })
+
+// function loadScript(url, callback){
+//     var head = document.getElementsByTagName('head')[0];
+//     var script = document.createElement('script');
+//     script.type = 'text/javascript';
+//     script.src = url;
+//     script.onreadystatechange = callback;
+//     script.onload = callback;
+//     head.appendChild(script);
+// }
+
+// loadScript('./hamburger.js', MiArchivoCargado);
+// function MiArchivoCargado(){
+//     alert('hamburger.js ya ha sido cargado');
+// }
+
+// function importarScript(nombre, callback) {
+//     var s = document.createElement("script");
+//     s.onload = callback;
+//     s.src = nombre;
+//     document.querySelector("head").appendChild(s);
+// }
+// importarScript("hamburger.js", scriptCargado);
+// x = 4;
+// function scriptCargado() { 
+//     console.log(x); 
+// }
+
